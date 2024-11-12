d795ecec8b3b87b377d19920dc935ca39460801c
Fabian <f.a.gutierrez@hotmail.com>
2024-11-12 09:07:09 -0300

    con imagenes falta borde

--- app/views.py
+++ app/views.py
@@ -5,13 +5,14 @@ from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+
 def index_page(request):
     return render(request, 'index.html')
 
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
+    images = services.getAllImages()
     favourite_list = []
 
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
@@ -22,7 +23,9 @@ def search(request):
     # si el texto ingresado no es vacío, trae las imágenes y favoritos desde services.py,
     # y luego renderiza el template (similar a home).
     if (search_msg != ''):
-        pass
+        images = services.getAllImages(search_msg)  # Filtra imágenes según el término
+        favourite_list = []
+        return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list})
     else:
         return redirect('home')
 
